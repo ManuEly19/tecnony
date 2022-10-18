bb802e9ded945238680fd16acf8883d4089046ac
Manu <manuelauqui@outlook.com>
2022-10-17 22:19:44 -0500

    Se agrego 3 tablas en la base de datos

---
+++ database/migrations/2022_10_17_214409_create_services_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            // ID para la tabla de la BDD
+            $table->id();
+
+            // columna para la tabla BDD
+            $table->string('name', 30);
+            $table->string('description', 100);
+            $table->float('price', 8, 2);
+            $table->boolean('state')->default(true);
+
+            // Relación de uno a mucho
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+
+            // Un usuario técnico proporciona muchos servicio y un servicio le pertenece a un técnico
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            // columnas especiales para la tabla de la BDD
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+};
