cf75b413c20c3b126f3e559552f518843529e658
Manuel Auqui <manuel.auqui@epn.edu.ec>
2023-02-04 20:12:30 -0500

    Nuevo page inicial

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://photos.app.goo.gl/1zynqCNdpRqtN8PL9" class="logo" alt="Laravel Logo">
+<img src="https://photos.app.goo.gl/1zynqCNdpRqtN8PL9">
 @else
 {{ $slot }}
 @endif
