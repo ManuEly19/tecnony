b4be3a7a32c918b7e9d3e5c336d5886cb58356fe
Manuel Auqui <manuel.auqui@epn.edu.ec>
2023-02-04 20:57:03 -0500

    Nuevo page inicial

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Tecnony')
 <img src="https://res.cloudinary.com/dlzylh5f6/image/upload/v1675559741/logo/Group_135_ixamwq.png" alt="Tecnony Logo">
 @else
 {{ $slot }}
