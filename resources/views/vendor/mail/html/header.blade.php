6b9a6369a6e0a564c76b937f098dbd2c345b9b9d
Manuel Auqui <manuel.auqui@epn.edu.ec>
2023-02-12 20:36:03 -0500

    Ultimo deploy

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="https://tecnony-v1.herokuapp.com" style="display: inline-block;">
 @if (trim($slot) === 'Tecnony')
 <img src="https://res.cloudinary.com/dlzylh5f6/image/upload/v1675559741/logo/Group_135_ixamwq.png" alt="Tecnony Logo">
 @else
